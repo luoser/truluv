824b551591b49d1ea22451b6fca805404201d976
luolisa <lisa@dali.dartmouth.edu>
2014-07-12 16:57:58 -0400

    refactoring boostrap

--- app/models/crawler.rb
+++ app/models/crawler.rb
@@ -3,8 +3,9 @@ require 'open-uri'
 class Crawler
 	BASE_CRAIGSLIST_URL = 'http://newyork.craigslist.org/search/mis/?query=m4w'
 
-	# page = Nokogiri::HTML(open(BASE_CRAIGSLIST_URL))
+	def crawl
+	  page = Nokogiri::HTML(open(BASE_CRAIGSLIST_URL))
+	end
 
-	
 
 end
