0a6879e3ab2a19dfffadc9d035e26abeabf3c809
luolisa <lisa@dali.dartmouth.edu>
2014-07-12 18:41:12 -0400

    begin crawler

---
+++ lib/crawler.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'open-uri'
+require 'nokogiri'
+
+module Crawler
+	# BASE_CRAIGSLIST_URL = 'http://newyork.craigslist.org/search/mis/?query=m4w'
+
+	# def crawl
+	#   page = Nokogiri::HTML(open(BASE_CRAIGSLIST_URL))
+	# end
+
+end
