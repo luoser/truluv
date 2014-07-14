67c57486ea401ab22ceed3e81d92cc8420561379
luolisa <lisa@dali.dartmouth.edu>
2014-07-13 21:04:32 -0400

    modified crawler

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+	require 'rubygems'
 	require 'open-uri'
 	require 'nokogiri'
 
@@ -21,7 +22,7 @@ class HomeController < ApplicationController
 				remote_url = root_url + href
 				@remote_urls << root_url
 
-				tdoc = Nokogiri::HTML(open(remote_url))
+				tdoc = Nokogiri::HTML(open(remote_url, 'User-Agent' => 'ruby'))
 				@blurbs << tdoc.at_css('#postingbody').text
 
 			end
