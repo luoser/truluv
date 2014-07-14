96ae510f3256589c2984533eecad6040891fca4a
luolisa <lisa@dali.dartmouth.edu>
2014-07-13 23:25:02 -0400

    testing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,23 +10,23 @@ class HomeController < ApplicationController
 		@blurbs = Array.new
 		# @scrape = Crawler.new(url)
 		
-		doc = Nokogiri::HTML(open(url, 'User-Agent' => 'ruby'))
-		rows = doc.css('.row')
+		# doc = Nokogiri::HTML(open(url, 'User-Agent' => 'ruby'))
+		# rows = doc.css('.row')
 
-		puts doc.at_css('title').text
+		# puts doc.at_css('title').text
 
-		rows[1..10].each do |row|
-			hrefs = row.css('a').map{ |x|
-				x['href']}.compact.uniq
-			hrefs.each do |href|
-				remote_url = root_url + href
-				@remote_urls << root_url
+		# rows[1..10].each do |row|
+		# 	hrefs = row.css('a').map{ |x|
+		# 		x['href']}.compact.uniq
+		# 	hrefs.each do |href|
+		# 		remote_url = root_url + href
+		# 		@remote_urls << root_url
 
-				tdoc = Nokogiri::HTML(open(remote_url, 'User-Agent' => 'ruby'))
-				@blurbs << tdoc.at_css('#postingbody').text
+		# 		tdoc = Nokogiri::HTML(open(remote_url, 'User-Agent' => 'ruby'))
+		# 		@blurbs << tdoc.at_css('#postingbody').text
 
-			end
-		end
+		# 	end
+		# end
 	end
 
 end
