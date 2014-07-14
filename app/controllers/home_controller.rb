724ed144e7febb2f820caf318813051c61e873b7
luolisa <lisa@dali.dartmouth.edu>
2014-07-13 23:33:35 -0400

    crawler back in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,25 +8,25 @@ class HomeController < ApplicationController
 		url = 'http://newyork.craigslist.org/search/mis/?query=m4w'
 		@remote_urls = Array.new
 		@blurbs = Array.new
-		# @scrape = Crawler.new(url)
+		@scrape = Crawler.new(url)
 		
-		# doc = Nokogiri::HTML(open(url, 'User-Agent' => 'ruby'))
-		# rows = doc.css('.row')
+		doc = Nokogiri::HTML(open(url, 'User-Agent' => 'ruby'))
+		rows = doc.css('.row')
 
-		# puts doc.at_css('title').text
+		puts doc.at_css('title').text
 
-		# rows[1..10].each do |row|
-		# 	hrefs = row.css('a').map{ |x|
-		# 		x['href']}.compact.uniq
-		# 	hrefs.each do |href|
-		# 		remote_url = root_url + href
-		# 		@remote_urls << root_url
+		rows[1..10].each do |row|
+			hrefs = row.css('a').map{ |x|
+				x['href']}.compact.uniq
+			hrefs.each do |href|
+				remote_url = root_url + href
+				@remote_urls << root_url
 
-		# 		tdoc = Nokogiri::HTML(open(remote_url, 'User-Agent' => 'ruby'))
-		# 		@blurbs << tdoc.at_css('#postingbody').text
+				tdoc = Nokogiri::HTML(open(remote_url, 'User-Agent' => 'ruby'))
+				@blurbs << tdoc.at_css('#postingbody').text
 
-		# 	end
-		# end
+			end
+		end
 	end
 
 end
