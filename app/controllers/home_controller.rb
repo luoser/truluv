c237ad5ce084a42d806e41b3b4ff9d8f847508ae
luolisa <lisa@dali.dartmouth.edu>
2014-07-22 23:08:48 -0400

    dumb. reverted.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,36 +1,32 @@
 class HomeController < ApplicationController
-	SCRAPED = false
-
+	
 	def index
 		root_url = 'http://newyork.craigslist.org/'
 		url = 'http://newyork.craigslist.org/search/mis/?query=m4w'
 		@remote_urls = Array.new
 		@blurbs = Array.new
 		
-		unless SCRAPED
-			begin
-				doc = Nokogiri::HTML(open(url, 'User-Agent' => 'ruby'))
-				rows = doc.css('.row')
-			rescue
-				flash[:notice] = 'Could not open root url.'
-				render :nothing => true
-			end
+		begin
+			doc = Nokogiri::HTML(open(url, 'User-Agent' => 'ruby'))
+			rows = doc.css('.row')
+		rescue
+			flash[:notice] = 'Could not open root url.'
+			render :nothing => true
+		end
 
-			if rows
-				rows[1..11].each do |row|
-					hrefs = row.css('a').map{ |x|
-						x['href']}.compact.uniq
-					hrefs.each do |href|
-						remote_url = root_url + href
-						@remote_urls << root_url
+		if rows
+			rows[1..11].each do |row|
+				hrefs = row.css('a').map{ |x|
+					x['href']}.compact.uniq
+				hrefs.each do |href|
+					remote_url = root_url + href
+					@remote_urls << root_url
 
-						tdoc = Nokogiri::HTML(open(remote_url, 'User-Agent' => 'ruby'))
-						@blurbs << tdoc.at_css('#postingbody').text
+					tdoc = Nokogiri::HTML(open(remote_url, 'User-Agent' => 'ruby'))
+					@blurbs << tdoc.at_css('#postingbody').text
 
-					end
 				end
 			end
-			SCRAPED = true
 		end
 	end
 
