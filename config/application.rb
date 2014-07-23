36c7d2ae95834ac7816b2b03889a4de3f78295f0
luolisa <lisa@dali.dartmouth.edu>
2014-07-22 22:48:37 -0400

    changed order of require

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'nokogiri'
 require 'open-uri'
+require 'nokogiri'
 
 
 # Require the gems listed in Gemfile, including any gems
