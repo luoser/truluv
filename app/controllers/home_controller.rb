f07225803e6d54779cbb35dbe93ec1fa0300e276
luolisa <lisa@dali.dartmouth.edu>
2014-07-23 00:22:17 -0400

    rescue

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,19 +9,23 @@ class HomeController < ApplicationController
 		@remote_urls = Array.new
 		@blurbs = Array.new
 		
-		doc = Nokogiri::HTML(open(url, 'User-Agent' => 'ruby'))
-		rows = doc.css('.row')
+		begin
+			doc = Nokogiri::HTML(open(url, 'User-Agent' => 'ruby'))
+			rows = doc.css('.row')
 
-		rows[1..11].each do |row|
-			hrefs = row.css('a').map{ |x|
-				x['href']}.compact.uniq
-			hrefs.each do |href|
-				remote_url = root_url + href
-				@remote_urls << root_url
+			rows[1..11].each do |row|
+				hrefs = row.css('a').map{ |x|
+					x['href']}.compact.uniq
+				hrefs.each do |href|
+					remote_url = root_url + href
+					@remote_urls << root_url
 
-				tdoc = Nokogiri::HTML(open(remote_url, 'User-Agent' => 'ruby'))
-				@blurbs << tdoc.at_css('#postingbody').text
+					tdoc = Nokogiri::HTML(open(remote_url, 'User-Agent' => 'ruby'))
+					@blurbs << tdoc.at_css('#postingbody').text
+				end
 			end
+		rescue
+			render :nothing => true
 		end
 	end
 
