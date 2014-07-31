2ba221994ca57e4b7a37ba4671abb7d410e9b7f1
luolisa <lisa@dali.dartmouth.edu>
2014-07-31 01:07:18 -0400

    app config

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Truluv
   class Application < Rails::Application
     config.assets.version = '1.0'
     config.action_controller.perform_caching = true
+    config.secret_key_base = '37e600e25d73feac0d91db1e5ec06c34aba7283990cdfca3acec2521cdedf5839c7b9914110f652a33ace036f79d1bab9b5ed7981bf07d3de28b995ac2e189df'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
